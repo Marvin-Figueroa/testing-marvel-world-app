d254e1086ec32d8e9c8792e25f1213dd41b68689
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-27 21:22:35 -0600

    Add tests for the Pagination component

---
+++ src/__tests__/Pagination.test.tsx
@@ -0,0 +1,72 @@
+import { render, screen } from '@testing-library/react';
+import Pagination from '../components/Pagination';
+
+describe('Pagination Component', () => {
+  it('should render a list of buttons with a left arrow, numbers from 1 to 5 and a right arrow', () => {
+    // Render
+    render(
+      <Pagination
+        pageSize={20}
+        siblingCount={1}
+        totalItems={100}
+        onPageChange={jest.fn()}
+        currentPage={1}
+      />
+    );
+    // Assertions
+    expect(screen.queryByText('1')).toBeInTheDocument();
+    expect(screen.queryByText('3')).toBeInTheDocument();
+    expect(screen.queryByText('5')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /next/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /previous/i })
+    ).toBeInTheDocument();
+  });
+
+  it('should not render anything', () => {
+    // Render
+    render(
+      <Pagination
+        pageSize={20}
+        siblingCount={1}
+        totalItems={20}
+        onPageChange={jest.fn()}
+        currentPage={1}
+      />
+    );
+    // Assertions
+    expect(screen.queryByText('1')).toBeNull;
+    expect(screen.queryByRole('button', { name: /next/i })).toBeNull();
+    expect(screen.queryByRole('button', { name: /previous/i })).toBeNull();
+  });
+
+  it('should render 1 button with 3 dots between the rest of the numbers', () => {
+    // Render
+    render(
+      <Pagination
+        pageSize={10}
+        siblingCount={1}
+        totalItems={100}
+        onPageChange={jest.fn()}
+        currentPage={1}
+      />
+    );
+    // Assertions
+    expect(screen.queryAllByTestId('dots')).toHaveLength(1);
+  });
+
+  it('should render 2 buttons with 3 dots among the rest of the numbers', () => {
+    // Render
+    render(
+      <Pagination
+        pageSize={5}
+        siblingCount={1}
+        totalItems={100}
+        onPageChange={jest.fn()}
+        currentPage={10}
+      />
+    );
+    // Assertions
+    expect(screen.queryAllByTestId('dots')).toHaveLength(2);
+  });
+});
