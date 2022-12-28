b1a0d1958d5058b989ace75050ed48a9f6265340
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-27 21:39:32 -0600

    Remove wrapping with BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import CharacterPage from './components/CharacterPage';
 import ComicPage from './components/ComicPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import StoryPage from './components/StoryPage';
 import NotFound from './components/NotFound';
@@ -13,20 +13,18 @@ import Bookmarks from './components/Bookmarks';
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<CharacterPage />} />
-          <Route path='/characters' element={<CharacterPage />} />
-          <Route path='/characters/:id' element={<CharacterDetail />} />
-          <Route path='/comics' element={<ComicPage />} />
-          <Route path='/comics/:id' element={<ComicDetail />} />
-          <Route path='/stories' element={<StoryPage />} />
-          <Route path='/stories/:id' element={<StoryDetail />} />
-          <Route path='/bookmarks' element={<Bookmarks />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<CharacterPage />} />
+        <Route path='/characters' element={<CharacterPage />} />
+        <Route path='/characters/:id' element={<CharacterDetail />} />
+        <Route path='/comics' element={<ComicPage />} />
+        <Route path='/comics/:id' element={<ComicDetail />} />
+        <Route path='/stories' element={<StoryPage />} />
+        <Route path='/stories/:id' element={<StoryDetail />} />
+        <Route path='/bookmarks' element={<Bookmarks />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
